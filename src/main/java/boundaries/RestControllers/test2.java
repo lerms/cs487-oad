1419361b95877ef6c2cef39aca872a9e1807bf4b
Jeannius <jeanrodneylarrieux@gmail.com>
2016-10-02 23:23:18 -0500

    added spring boot to the project. Added a test class than returns hello world

---
+++ src/main/java/boundaries/RestControllers/test2.java
@@ -0,0 +1,30 @@
+package boundaries.RestControllers;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ * Created by Jeannius on 10/2/2016.
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class test2 {
+
+
+    @RequestMapping("/")
+    String home(){
+        return  "hello world";
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(test2.class, args);
+    }
+}
---
+++ src/main/java/boundaries/test.java
@@ -0,0 +1,11 @@
+package boundaries;
+
+
+
+/**
+ * Created by Jeannius on 10/2/2016.
+ */
+public class test {
+
+
+}

7c13f943998bcf20f63f50ed7c71eda0d79454b8
Patrick Mendus <pmendus@hawk.iit.edu>
2016-11-27 21:57:04 -0600

    Changed listeners to onload instead of ready

--- CS487_OAD_Website/js/oad_menu_display.js
+++ CS487_OAD_Website/js/oad_menu_display.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 
+/*
 //FIND TOGGLE
 $(document).ready(function(){
 	var find_wrapper_toggle = 1;
@@ -53,4 +54,54 @@ $(document).ready(function(){
 			$(this).attr("src", "images/plus_button.png");
 		}
 	});
-});
+});*/
+
+function listener(){
+	var find_wrapper_toggle = 1;
+	$("#arrow").click(function(){
+		if($("#find_wrapper").css("display") === ("none")){
+			$("#find_wrapper").show();
+			$(this).attr("src", "images/up_button.png");
+			find_wrapper_toggle = 0;
+		} else {
+			$("#find_wrapper").hide();
+			$(this).attr("src", "images/down_button.png");
+			find_wrapper_toggle = 1;
+		}
+	});
+
+//SUBCATEGORY TOGGLE
+	$(".category_show_button").click(function(){
+		if($(this).parent().next(".category_subcategories").css("display") === ("none")){
+			$(this).parent().next(".category_subcategories").show();
+			$(this).attr("src", "images/minus_button.png");
+		} else {
+			$(this).parent().next(".category_subcategories").hide();
+			$(this).attr("src", "images/plus_button.png");
+		}
+	});
+
+//LISTING TOGGLE
+	$(".subcategory_show_button").click(function(){
+		if($(this).parent().next(".listings").css("display") === ("none")){
+			$(this).parent().next(".listings").show();
+			$(this).attr("src", "images/minus_button.png");
+		} else {
+			$(this).parent().next(".listings").hide();
+			$(this).attr("src", "images/plus_button.png");
+		}
+	});
+
+//LISTING DESCRIPTION TOGGLE
+	$(".listing_show_button").click(function(){
+		if($(this).parent().next(".listing_bottom_wrapper").css("display") === ("none")){
+			$(this).parent().next(".listing_bottom_wrapper").show();
+			$(this).attr("src", "images/minus_button.png");
+		} else {
+			$(this).parent().next(".listing_bottom_wrapper").hide();
+			$(this).attr("src", "images/plus_button.png");
+		}
+	});
+}
+
+window.onload = listener();

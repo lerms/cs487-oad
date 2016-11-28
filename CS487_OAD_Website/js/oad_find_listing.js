f8fc5b3f4a223e9c7843a1e72f22ba51953c9a04
Patrick Mendus <pmendus@hawk.iit.edu>
2016-11-28 14:43:01 -0600

    Fixed buttons and strange find bugs

--- CS487_OAD_Website/js/oad_find_listing.js
+++ CS487_OAD_Website/js/oad_find_listing.js
@@ -78,6 +78,8 @@ function findListings(){
 	$.get(url+"/listing?search=" + listing + ",city=" + city + ",neighborhood=" + neighborhood, function(data, status){
 		//TODO CLEAR AND POPULATE LISTINGS
 		$(categories).empty();
+		$(home_featured_listing).empty();
+		$(featured_list).empty();
 		createHTML2(data);
 	});
 	
